8d0214615b7a9299fb0faf5970e1bf249cb5662f
mohadeseh chopani <chopanimohadeseh0@gmail.com>
2023-09-05 10:16:21 +0330

    Set UI for application

---
+++ app/src/main/java/com/example/todolist/Details/DetailsActivity.java
@@ -0,0 +1,16 @@
+package com.example.todolist.Details;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.todolist.R;
+
+public class DetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+    }
+}
---
+++ app/src/main/java/com/example/todolist/Main/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.todolist.Main;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toolbar;
+
+import com.example.todolist.DialogAdd;
+import com.example.todolist.DialogEdit;
+import com.example.todolist.Main.Tasks_adapter;
+import com.example.todolist.Models.DataDao;
+import com.example.todolist.Models.Data_task;
+import com.example.todolist.Models.Database_holder;
+import com.example.todolist.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
